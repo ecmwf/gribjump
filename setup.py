a9515c7839e4508c8519ac231af186fd2fe3f423
Chris Bradley <chris.bradley@ecmwf.int>
2023-12-14 01:06:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ import re
 from setuptools import find_packages, setup
 
 __version__ = re.search(
-    r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', io.open("pyfdb/version.py", encoding="utf_8_sig").read()
+    r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', io.open("pygribjump/version.py", encoding="utf_8_sig").read()
 ).group(1)
 
 setup(
     name="pygribjump",
     version=__version__,
-    description="Python interface to FDB",
+    description="Python interface to GribJump",
     url="https://github.com/ecmwf/gribjump",
     author="ECMWF",
     author_email="software.support@ecmwf.int",
