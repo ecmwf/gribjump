3e6d9c55660c67665a71247a874f92f45fd4949c
Eugen Betke <eugen.betke@ecmwf.int>
2024-01-24 21:26:32 +0000

    SimplePacking: fix bad memory access

--- src/gribjump/compression/compressors/Simple.h
+++ src/gribjump/compression/compressors/Simple.h
@@ -16,14 +16,12 @@ namespace mc {
 
 
 template <typename ValueType>
-size_t bin_pos(size_t value_idx, size_t bits_per_value)
+size_t chunk_offset(size_t value_idx, size_t bits_per_value)
 {
-  constexpr size_t chunk_nvals = 8;
-  size_t chunk_size = bits_per_value;
-  size_t chunk_idx = value_idx / chunk_nvals;
-  size_t value_idx_first_in_chunk = chunk_idx * chunk_nvals;
-  size_t chunk_offset_bytes = value_idx_first_in_chunk * bits_per_value / chunk_nvals;
-
+  size_t chunk_idx = value_idx / 8;
+  size_t value_idx_first_in_chunk = chunk_idx * 8;
+  assert(value_idx_first_in_chunk * bits_per_value % 8 == 0); // First value in a chunk starts always at a byte boundary
+  size_t chunk_offset_bytes = value_idx_first_in_chunk * bits_per_value / 8;
   return chunk_offset_bytes;
 }
 
@@ -100,31 +98,38 @@ public:
     using SP = SimplePacking<ValueType>;
     SimplePacking<double> sp{};
     DecodeParameters<ValueType> params{};
-    constexpr int VSIZE = sizeof(ValueType);
 
+    // SimplePacking parameters
     auto [offset, size] = range;
+
+    // Optimization:
+    // Requested values can start and end in the middle of a byte.
+    // Chunks of 8 values start always at a byte boundary.
+    size_t new_offset = offset / 8 * 8;
+    size_t end = offset + size;
+    size_t new_end = (end + 7) / 8 * 8;
+    size_t new_size = new_end - new_offset;
+    Range inclusive_range{new_offset, new_size}; // Includes extra values before and after the requested range
+
+    size_t shift_offset = offset - new_offset; // Extra values before the requested range
+
     params.bits_per_value = bits_per_value_;
     params.reference_value = reference_value_;
     params.binary_scale_factor = binary_scale_factor_;
     params.decimal_scale_factor = decimal_scale_factor_;
+    params.n_vals = new_size; // Includes extra values before and after the requested range
 
-    constexpr size_t chunk_nvals = 8;
-    size_t new_offset = offset / chunk_nvals * chunk_nvals;
-    size_t end = offset + size;
-    size_t new_end = (end + (chunk_nvals - 1)) / chunk_nvals * chunk_nvals;
-    size_t new_size = new_end - new_offset;
-    Range inclusive_range{new_offset, new_size};
-
-    params.n_vals = new_size;
-
-    size_t last_pos = std::min(bin_pos<ValueType>(new_size, bits_per_value_), accessor->eof() - bin_pos<ValueType>(new_offset, bits_per_value_));
-    Range data_range{bin_pos<ValueType>(new_offset, bits_per_value_), last_pos};
+    // Byte level
+    size_t begin_bytes = chunk_offset<ValueType>(new_offset, bits_per_value_);
+    size_t end_bytes = std::min(chunk_offset<ValueType>(new_end, bits_per_value_), accessor->eof());
+    Range data_range{begin_bytes, end_bytes - begin_bytes};
     eckit::Buffer compressed = accessor->read(data_range);
 
     typename SP::Buffer data{(unsigned char*) compressed.data(), (unsigned char*) compressed.data() + data_range.second};
     typename SP::Values decoded = sp.unpack(params, data);
 
-    size_t shift_offset = offset - new_offset;
+    assert(shift_offset + size <= decoded.size());
+
     Values out_values{decoded.data() + shift_offset, decoded.data() + shift_offset + size};
 
     return out_values;
