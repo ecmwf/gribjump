0a97664f6fbe288499c5770af62846abdaa972c7
Chris Bradley <chris.bradley@ecmwf.int>
2024-10-07 11:33:35 +0100

    update examples

--- pygribjump/example_extract.py
+++ pygribjump/example_extract.py
@@ -1,6 +1,7 @@
 import os
 os.environ['GRIBJUMP_HOME'] = '/path/to/gribjump/build/'
 os.environ['GRIBJUMP_CONFIG_FILE'] = "/path/to/gribjump/config.yaml"
+os.environ['GRIBJUMP_IGNORE_GRID'] = "1"
 
 import pygribjump as pygj
 
