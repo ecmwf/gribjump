14c428cdac1d7bbe4dd3eaf9d22a9d571849d428
Kai Kratz <Kai.Kratz@ecmwf.int>
2024-10-10 08:51:38 +0200

    Add first python api test

--- pygribjump/tests/test_pygribjump.py
+++ pygribjump/tests/test_pygribjump.py
@@ -1,5 +1,85 @@
-import pytest
-import pygribjump as pygj
+"""
+NOTE:
 
-def test_dummy():
-    pass
+To be able to run the tests pyfdb and pygribjump need to find the native libraries.
+If not installed into a default location export the following environent
+variables in the shell running pytest:
+    GRIBJUMP_HOME = <cmake-build-root-for-gribjump> or <custom-install-location>
+    FDB5_DIR = <cmake-build-root-for-fdb> or <custom-install-location>
+"""
+
+import os
+import pathlib
+import shutil
+
+import numpy
+import pytest
+import yaml
+
+# Do not reorder the imports otherwise this will trigger an assertion in eckits
+# library registration handling.
+import pygribjump as pygj
+import pyfdb
+
+
+@pytest.fixture(scope="function")
+def read_only_fdb_setup(data_path, tmp_path) -> pathlib.Path:
+    """
+    Creates a FDB setup in this tests temp directory.
+    Test FDB currently reads all grib files in `tests/data`
+    """
+    db_store_path = tmp_path / "db_store"
+    db_store_path.mkdir(exist_ok=True)
+    schema_path = tmp_path / "schema"
+    config = dict(
+        type="local",
+        engine="toc",
+        schema=str(schema_path),
+        spaces=[
+            dict(
+                handler="Default",
+                roots=[
+                    {"path": str(db_store_path)},
+                ],
+            )
+        ],
+    )
+    config_path = tmp_path / "config.yaml"
+    config_path.write_text(yaml.dump(config))
+    shutil.copy(data_path / "schema", schema_path)
+    os.environ["FDB5_CONFIG_FILE"] = str(config_path)
+
+    # grib_files = data_path.glob("*.grib")
+    grib_files = [data_path / "synth11.grib"]
+    fdb = pyfdb.FDB()
+    for f in grib_files:
+        fdb.archive(f.read_bytes())
+    return tmp_path
+
+
+def test_extract_simple_sunshine_case(read_only_fdb_setup) -> None:
+    grib_jump = pygj.GribJump()
+    reqstrs = [
+        {
+            "domain": "g",
+            "levtype": "sfc",
+            "date": "20230508",
+            "time": "1200",
+            "step": "1",
+            "param": "151130",
+            "class": "od",
+            "type": "fc",
+            "stream": "oper",
+            "expver": "0001",
+        },
+    ]
+    ranges = [
+        [(0, 6)],
+    ]
+
+    polyrequest = [(a, b) for a, b in zip(reqstrs, ranges)]
+
+    expected = numpy.array([0.0, numpy.nan, numpy.nan, 3.0, 4.0, numpy.nan])
+
+    actual = grib_jump.extract(polyrequest)
+    assert numpy.array_equal(expected, actual[0][0][0][0], equal_nan=True)
