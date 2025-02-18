87734c1d729282b1339aed508853c25adb7ca9ea
Chris Bradley <chris.bradley@ecmwf.int>
2025-02-18 18:28:19 +0000

    Move ccsds logic out of header file. Some tidy up

--- src/gribjump/compression/compressors/Ccsds.cc
+++ src/gribjump/compression/compressors/Ccsds.cc
@@ -10,32 +10,205 @@
 
 #include "Ccsds.h"
 #include "Aec.h"
-//#include "serializer.h"
-//#include "libaec.h"
-
-#include <unordered_map>
 #include <memory>
-#include <cassert>
 #include <algorithm>
 
-namespace mc {
+namespace  {
 
-bool is_big_endian()
-{
+/* Return n to the power of s */
+template <typename T>
+constexpr T codes_power(long s, long n) {
+    T divisor = 1.0;
+    if (s == 0)
+        return 1.0;
+    if (s == 1)
+        return n;
+    while (s < 0) {
+        divisor /= n;
+        s++;
+    }
+    while (s > 0) {
+        divisor *= n;
+        s--;
+    }
+    return divisor;
+}
+
+bool is_big_endian() {
     unsigned char is_big_endian = 0;
     unsigned short endianess_test = 1;
     return reinterpret_cast<const char*>(&endianess_test)[0] == is_big_endian;
 }
 
-size_t modify_aec_flags(size_t flags)
-{
-  // ECC-1602: Performance improvement: enabled the use of native data types
-  flags &= ~AEC_DATA_3BYTE;  // disable support for 3-bytes per value
-  if (is_big_endian())
-    flags |= AEC_DATA_MSB; // enable big-endian
-  else
-    flags &= ~AEC_DATA_MSB;  // enable little-endian
-  return flags;
+size_t modify_aec_flags(size_t flags) {
+    // ECC-1602: Performance improvement: enabled the use of native data types
+    flags &= ~AEC_DATA_3BYTE;  // disable support for 3-bytes per value
+    if (is_big_endian())
+        flags |= AEC_DATA_MSB; // enable big-endian
+    else
+        flags &= ~AEC_DATA_MSB;  // enable little-endian
+    return flags;
 }
+
+} // namespace
+
+namespace mc {
+
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename ValueType>
+CcsdsParams::Offsets CcsdsDecompressor<ValueType>::decode_offsets(const eckit::Buffer& in_buf){
+
+    Offsets offsets;
+    switch (auto nbytes = sample_nbytes()) {
+        case 1:
+            offsets = decode_offsets_<uint8_t>(in_buf);
+            break;
+        case 2:
+            offsets = decode_offsets_<uint16_t>(in_buf);
+            break;
+        case 4:
+            offsets = decode_offsets_<uint32_t>(in_buf);
+            break;
+        default:
+            std::stringstream ss;
+            ss << nbytes;
+            throw eckit::SeriousBug("Invalid number of bytes per sample: " + ss.str(), Here());
+    }
+
+    return offsets;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename ValueType>
+typename CcsdsDecompressor<ValueType>::Values CcsdsDecompressor<ValueType>::decode(const eckit::Buffer& in_buf) {
+    auto bscale = codes_power<double>(binary_scale_factor_, 2);
+    auto dscale = codes_power<double>(decimal_scale_factor_, -10);
+    auto flags = modify_aec_flags(flags_);
+
+    Values values;
+
+    switch (auto nbytes = sample_nbytes()) {
+        case 1:
+            values = decode_all_<uint8_t>(in_buf, bscale, dscale);
+            break;
+        case 2:
+            values = decode_all_<uint16_t>(in_buf, bscale, dscale);
+            break;
+        case 4:
+            values = decode_all_<uint32_t>(in_buf, bscale, dscale);
+            break;
+        default:
+            std::stringstream ss;
+            ss << nbytes;
+            throw eckit::SeriousBug("Invalid number of bytes per sample: " + ss.str(), Here());
+    }
+
+    return values;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename ValueType>
+typename CcsdsDecompressor<ValueType>::Values CcsdsDecompressor<ValueType>::decode(const std::shared_ptr<DataAccessor> accessor, const Block& range){
+    if (range.second == 0) {
+        return Values{};
+    }
+    
+    auto bscale = codes_power<double>(binary_scale_factor_, 2);
+    auto dscale = codes_power<double>(decimal_scale_factor_, -10);
+    Values values;
+
+    switch (auto nbytes = sample_nbytes()) {
+        case 1:
+        values = decode_range_<uint8_t>(accessor, range, bscale, dscale);
+        break;
+        case 2:
+        values = decode_range_<uint16_t>(accessor, range, bscale, dscale);
+        break;
+        case 4:
+        values = decode_range_<uint32_t>(accessor, range, bscale, dscale);
+        break;
+        default:
+        std::stringstream ss;
+        ss << nbytes;
+        throw eckit::SeriousBug("Invalid number of bytes per sample: " + ss.str(), Here());
+    }
+
+    return values;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename ValueType>
+size_t CcsdsDecompressor<ValueType>::sample_nbytes() const { 
+    size_t nbytes = (bits_per_sample_ + 7) / 8;
+    if (nbytes == 3) {
+        nbytes = 4;
+    }
+    return nbytes;
+}
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename ValueType>
+template <typename SimpleValueType>
+typename CcsdsDecompressor<ValueType>::Values CcsdsDecompressor<ValueType>::decode_range_ (const std::shared_ptr<DataAccessor> accessor, const Block& simple_range, double bscale, double dscale) {
+    AecDecompressor<SimpleValueType> aec{};
+    auto flags = modify_aec_flags(flags_);
+    aec.flags(flags);
+    aec.bits_per_sample(bits_per_sample_);
+    aec.block_size(block_size_);
+    aec.rsi(rsi_);
+    aec.offsets(offsets_);
+
+    typename AecDecompressor<SimpleValueType>::Values simple_values = aec.decode(accessor, simple_range);
+    
+    Values values(simple_values.size());
+    std::transform(simple_values.begin(), simple_values.end(), values.begin(), [&](const auto& simple_value) {
+        return (simple_value * bscale + reference_value_) * dscale;
+    });
+
+    return values;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename ValueType>
+template <typename SimpleValueType>
+CcsdsParams::Offsets CcsdsDecompressor<ValueType>::decode_offsets_ (const typename AecDecompressor<SimpleValueType>::CompressedData& in_buf) {
+    AecDecompressor<SimpleValueType> aec{};
+    auto flags = modify_aec_flags(flags_);
+    aec.flags(flags);
+    aec.bits_per_sample(bits_per_sample_);
+    aec.block_size(block_size_);
+    aec.rsi(rsi_);
+    aec.n_elems(n_elems_);
+
+    return aec.decode_offsets(in_buf);
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename ValueType>
+template <typename SimpleValueType>
+typename CcsdsDecompressor<ValueType>::Values CcsdsDecompressor<ValueType>::decode_all_ (const typename AecDecompressor<SimpleValueType>::CompressedData& in_buf, double bscale, double dscale) {
+    AecDecompressor<SimpleValueType> aec{};
+    auto flags = modify_aec_flags(flags_);
+    aec.flags(flags);
+    aec.bits_per_sample(bits_per_sample_);
+    aec.block_size(block_size_);
+    aec.rsi(rsi_);
+    aec.n_elems(n_elems_);
+
+    typename AecDecompressor<SimpleValueType>::Values simple_values = aec.decode(in_buf);
+    
+    if (aec.offsets()) {
+      offsets_ = aec.offsets().value();
+    }
+
+    Values values(simple_values.size());
+    std::transform(simple_values.begin(), simple_values.end(), values.begin(), [&](const auto& simple_value) {
+        return (simple_value * bscale + reference_value_) * dscale;
+    });
+
+    return values;
+}
+// ---------------------------------------------------------------------------------------------------------------------
+// Explicit instantiations of the template class
+template class CcsdsDecompressor<double>;
 
 } // namespace mc
--- src/gribjump/compression/compressors/Ccsds.h
+++ src/gribjump/compression/compressors/Ccsds.h
@@ -26,307 +26,88 @@
 
 namespace mc {
 
-/* Return n to the power of s */
-template <typename T>
-constexpr T codes_power(long s, long n)
-{
-  T divisor = 1.0;
-  if (s == 0)
-    return 1.0;
-  if (s == 1)
-    return n;
-  while (s < 0) {
-    divisor /= n;
-    s++;
-  }
-  while (s > 0) {
-    divisor *= n;
-    s--;
-  }
-  return divisor;
-}
-
-
-bool is_big_endian();
-size_t modify_aec_flags(size_t flags);
-
-
 class CcsdsParams {
 public:
-  using Offsets = std::vector<size_t>;
-  CcsdsParams() : flags_(8ul), rsi_(128ul), block_size_(32ul), bits_per_sample_(16ul) {}
+    using Offsets = std::vector<size_t>;
+    CcsdsParams() : flags_(8ul), rsi_(128ul), block_size_(32ul), bits_per_sample_(16ul) {}
 
-  size_t flags() const { return flags_; }
-  size_t rsi() const { return rsi_; }
-  size_t block_size() const { return block_size_; }
-  size_t bits_per_sample() const { return bits_per_sample_; }
-  double reference_value() const { return reference_value_; }
-  size_t decimal_scale_factor() const { return decimal_scale_factor_; }
-  size_t binary_scale_factor() const { return binary_scale_factor_; }
-  std::optional<Offsets> offsets() const {
-    return offsets_.size() > 0 ? std::optional<Offsets>(offsets_) : std::nullopt;
-  }
+    size_t flags() const { return flags_; }
+    size_t rsi() const { return rsi_; }
+    size_t block_size() const { return block_size_; }
+    size_t bits_per_sample() const { return bits_per_sample_; }
+    double reference_value() const { return reference_value_; }
+    size_t decimal_scale_factor() const { return decimal_scale_factor_; }
+    size_t binary_scale_factor() const { return binary_scale_factor_; }
+    std::optional<Offsets> offsets() const {
+        return offsets_.size() > 0 ? std::optional<Offsets>(offsets_) : std::nullopt;
+    }
 
-  CcsdsParams& flags(size_t flags) { flags_ = flags; return *this; }
-  CcsdsParams& rsi(size_t rsi) { rsi_ = rsi; return *this; }
-  CcsdsParams& block_size(size_t block_size) { block_size_ = block_size; return *this; }
-  CcsdsParams& bits_per_sample(size_t bits_per_sample) { bits_per_sample_ = bits_per_sample; return *this; }
-  CcsdsParams& reference_value(double reference_value) { reference_value_ = reference_value; return *this; }
-  CcsdsParams& decimal_scale_factor(size_t decimal_scale_factor) { decimal_scale_factor_ = decimal_scale_factor; return *this; }
-  CcsdsParams& binary_scale_factor(size_t binary_scale_factor) { binary_scale_factor_ = binary_scale_factor; return *this; }
-  CcsdsParams& offsets(const Offsets& offsets) {
-    assert(offsets.size() > 0);
-    offsets_ = offsets;
-    return *this;
-  }
+    CcsdsParams& flags(size_t flags) { flags_ = flags; return *this; }
+    CcsdsParams& rsi(size_t rsi) { rsi_ = rsi; return *this; }
+    CcsdsParams& block_size(size_t block_size) { block_size_ = block_size; return *this; }
+    CcsdsParams& bits_per_sample(size_t bits_per_sample) { bits_per_sample_ = bits_per_sample; return *this; }
+    CcsdsParams& reference_value(double reference_value) { reference_value_ = reference_value; return *this; }
+    CcsdsParams& decimal_scale_factor(size_t decimal_scale_factor) { decimal_scale_factor_ = decimal_scale_factor; return *this; }
+    CcsdsParams& binary_scale_factor(size_t binary_scale_factor) { binary_scale_factor_ = binary_scale_factor; return *this; }
+    CcsdsParams& offsets(const Offsets& offsets) {
+        ASSERT(offsets.size() > 0);
+        offsets_ = offsets;
+        return *this;
+    }
 
 protected:
-  size_t flags_;
-  size_t rsi_;
-  size_t block_size_;
-  size_t bits_per_sample_;
-  double reference_value_;
-  size_t decimal_scale_factor_;
-  size_t binary_scale_factor_;
-  std::vector<size_t> offsets_;
+    size_t flags_;
+    size_t rsi_;
+    size_t block_size_;
+    size_t bits_per_sample_;
+    double reference_value_;
+    size_t decimal_scale_factor_;
+    size_t binary_scale_factor_;
+    std::vector<size_t> offsets_;
 };
-
-template <typename ValueType> class CcsdsCompressor;
-template <typename ValueType> class CcsdsDecompressor;
-
-template <typename ValueType>
-class CcsdsCompressor : public NumericCompressor<ValueType>, public CcsdsParams {
-public:
-  using CompressedData = typename NumericCompressor<ValueType>::CompressedData;
-  using Values = typename NumericCompressor<ValueType>::Values;
-  std::pair<std::unique_ptr<NumericDecompressor<ValueType>>, CompressedData> encode(const Values& values) override {
-    size_t nbytes = (bits_per_sample_ + 7) / 8;
-    if (nbytes == 3)
-      nbytes = 4;
-
-    CcsdsCompressor<ValueType> ccsds_decompressor{};
-    CcsdsCompressor<ValueType>::CompressedData data{};
-    switch (nbytes) {
-      case 1:
-        std::tie(ccsds_decompressor, data) = encode_all_<uint8_t>(values, nbytes);
-        break;
-      case 2:
-        std::tie(ccsds_decompressor, data) = encode_all_<uint16_t>(values, nbytes);
-        break;
-      case 4:
-        std::tie(ccsds_decompressor, data) = encode_all_<uint32_t>(values, nbytes);
-        break;
-      default:
-        std::stringstream ss;
-        ss << nbytes;
-        throw eckit::SeriousBug("Unsupported number of bytes per value: " + ss.str(), Here());
-    }
-
-    //return std::make_pair(std::move(ccsds_decompressor), std::move(data)); 
-    return std::make_pair(ccsds_decompressor, data); 
-  }
-
-private:
-  template <typename SimpleValueType>
-  std::pair<CcsdsDecompressor<ValueType>, CompressedData> encode_all_(const Values& values, size_t nbytes) {
-    ValueType divisor = codes_power<ValueType>(binary_scale_factor_, 2);
-    ValueType d = codes_power<ValueType>(decimal_scale_factor_, 10);
-    std::vector<SimpleValueType> simple_values(values.size());
-
-    std::transform(values.begin(), values.end(), simple_values.begin(), [&](const auto& value) {
-      return ((value * d - reference_value_) * divisor) + 0.5;
-    });
-
-    AecCompressor<ValueType> aec{};
-    aec.flags(flags_);
-    aec.bits_per_sample(bits_per_sample_);
-    aec.block_size(block_size_);
-    aec.rsi(rsi_);
-
-    auto [aec_decompressor_base, aec_data] = aec.encode(simple_values);
-    AecDecompressor<ValueType> aec_decompressor = dynamic_cast<AecDecompressor<ValueType>&>(*aec_decompressor_base);
-
-    std::unique_ptr<CcsdsDecompressor<ValueType>> ccsds_decompressor = std::make_unique<CcsdsDecompressor<ValueType>>();
-    ccsds_decompressor->flags(aec_decompressor.flags());
-    ccsds_decompressor->rsi(aec_decompressor.rsi());
-    ccsds_decompressor->block_size(aec_decompressor.block_size());
-    ccsds_decompressor->bits_per_sample(aec_decompressor.bits_per_sample());
-    ccsds_decompressor->reference_value(reference_value_);
-    ccsds_decompressor->decimal_scale_factor(decimal_scale_factor_);
-    ccsds_decompressor->binary_scale_factor(binary_scale_factor_);
-    if (aec_decompressor.offsets()) {
-      ccsds_decompressor->offsets(aec_decompressor.offsets().value());
-    }
-    ccsds_decompressor->n_elems(aec_decompressor.n_elems());
-    //return std::make_pair(std::move(ccsds_decompressor), std::move(aec_data));
-    return std::make_pair(ccsds_decompressor, aec_data);
-  }
-
-};
-
 
 template <typename ValueType>
 class CcsdsDecompressor : public mc::NumericDecompressor<ValueType>, public CcsdsParams {
-public:
-  using CompressedData = typename mc::NumericDecompressor<ValueType>::CompressedData;
-  using Values = typename mc::NumericDecompressor<ValueType>::Values;
 
-  Offsets decode_offsets(const CompressedData& in_buf) override {
-    // only decode the offsets, if at all possible.
+public:  // types
+    using CompressedData = typename mc::NumericDecompressor<ValueType>::CompressedData;
+    using Values = typename mc::NumericDecompressor<ValueType>::Values;
 
-    size_t nbytes = (bits_per_sample_ + 7) / 8;
-    if (nbytes == 3)
-      nbytes = 4;
+public:  // methods
+    using NumericDecompressor<ValueType>::decode;
 
-    size_t i;
-    Offsets offsets;
-    switch (nbytes) {
-      case 1:
-        offsets = decode_offsets_<uint8_t>(in_buf);
-        break;
-      case 2:
-        offsets = decode_offsets_<uint16_t>(in_buf);
-        break;
-      case 4:
-        offsets = decode_offsets_<uint32_t>(in_buf);
-        break;
-      default:
-        std::stringstream ss;
-        ss << nbytes;
-        throw eckit::SeriousBug("Invalid number of bytes per sample: " + ss.str(), Here());
-    }
+    /// Decode a compressed buffer
+    Values decode(const eckit::Buffer& in_buf) override;
 
-    return offsets;
-  }
+    /// Decode a range of values
+    Values decode(const std::shared_ptr<DataAccessor> accessor, const Block& range) override;
 
+    /// Avoid fully decoding the data, only decode the offsets
+    /// @note: libaec will still do its part of the decoding
+    Offsets decode_offsets(const eckit::Buffer& in_buf) override; 
 
-  Values decode(const CompressedData& in_buf) override
-  {
-    auto bscale = codes_power<double>(binary_scale_factor_, 2);
-    auto dscale = codes_power<double>(decimal_scale_factor_, -10);
-    auto flags = modify_aec_flags(flags_);
+    size_t n_elems() const { return n_elems_; }
+    CcsdsDecompressor& n_elems(size_t n_elems) { n_elems_ = n_elems; return *this; }
 
-    size_t nbytes = (bits_per_sample_ + 7) / 8;
-    if (nbytes == 3)
-      nbytes = 4;
-
-    Values values;
-
-    size_t i;
-    switch (nbytes) {
-      case 1:
-        values = decode_all_<uint8_t>(in_buf, bscale, dscale);
-        break;
-      case 2:
-        values = decode_all_<uint16_t>(in_buf, bscale, dscale);
-        break;
-      case 4:
-        values = decode_all_<uint32_t>(in_buf, bscale, dscale);
-        break;
-      default:
-        std::stringstream ss;
-        ss << nbytes;
-        throw eckit::SeriousBug("Invalid number of bytes per sample: " + ss.str(), Here());
-    }
-
-    return values;
-  }
-
-
-  Values decode(const std::shared_ptr<DataAccessor> accessor, const Block& range) override {
-    if (range.second == 0)
-      return Values{};
-
-    size_t simple_nbytes = (bits_per_sample_ + 7) / 8;
-    if (simple_nbytes == 3)
-      simple_nbytes = 4;
-
-    size_t value_nbytes = sizeof(double);
-    auto bscale = codes_power<double>(binary_scale_factor_, 2);
-    auto dscale = codes_power<double>(decimal_scale_factor_, -10);
-    Values values;
-
-    switch (simple_nbytes) {
-      case 1:
-        values = decode_range_<uint8_t>(accessor, range, bscale, dscale);
-        break;
-      case 2:
-        values = decode_range_<uint16_t>(accessor, range, bscale, dscale);
-        break;
-      case 4:
-        values = decode_range_<uint32_t>(accessor, range, bscale, dscale);
-        break;
-      default:
-        std::stringstream ss;
-        ss << simple_nbytes;
-        throw eckit::SeriousBug("Invalid number of bytes per sample: " + ss.str(), Here());
-    }
-
-    return values;
-  }
-
-  using NumericDecompressor<ValueType>::decode;
-
-  size_t n_elems() const { return n_elems_; }
-  CcsdsDecompressor& n_elems(size_t n_elems) { n_elems_ = n_elems; return *this; }
 
 private:
-  size_t n_elems_;
 
-  template <typename SimpleValueType>
-  Values decode_range_ (const std::shared_ptr<DataAccessor> accessor, const Block& simple_range, double bscale, double dscale) {
-    AecDecompressor<SimpleValueType> aec{};
-    auto flags = modify_aec_flags(flags_);
-    aec.flags(flags);
-    aec.bits_per_sample(bits_per_sample_);
-    aec.block_size(block_size_);
-    aec.rsi(rsi_);
-    aec.offsets(offsets_);
+    size_t sample_nbytes() const;
 
-    typename AecDecompressor<SimpleValueType>::Values simple_values = aec.decode(accessor, simple_range);
-    Values values(simple_values.size());
-    std::transform(simple_values.begin(), simple_values.end(), values.begin(), [&](const auto& simple_value) {
-                       return (simple_value * bscale + reference_value_) * dscale;
-                   });
-    return values;
-  }
 
-  template <typename SimpleValueType>
-    Offsets decode_offsets_ (const typename AecDecompressor<SimpleValueType>::CompressedData& in_buf) {
-    AecDecompressor<SimpleValueType> aec{};
-    auto flags = modify_aec_flags(flags_);
-    aec.flags(flags);
-    aec.bits_per_sample(bits_per_sample_);
-    aec.block_size(block_size_);
-    aec.rsi(rsi_);
-    aec.n_elems(n_elems_);
+    // Wrappers around aec.decode
 
-    return aec.decode_offsets(in_buf);
-  }
+    template <typename SimpleValueType>
+    Values decode_range_(const std::shared_ptr<DataAccessor> accessor, const Block& simple_range, double bscale, double dscale);
 
-  template <typename SimpleValueType>
-  Values decode_all_ (const typename AecDecompressor<SimpleValueType>::CompressedData& in_buf, double bscale, double dscale) {
-    AecDecompressor<SimpleValueType> aec{};
-    auto flags = modify_aec_flags(flags_);
-    aec.flags(flags);
-    aec.bits_per_sample(bits_per_sample_);
-    aec.block_size(block_size_);
-    aec.rsi(rsi_);
-    aec.n_elems(n_elems_);
+    template <typename SimpleValueType>
+    Offsets decode_offsets_(const typename AecDecompressor<SimpleValueType>::CompressedData& in_buf);
 
-    typename AecDecompressor<SimpleValueType>::Values simple_values = aec.decode(in_buf);
-    
-    if (aec.offsets()) {
-      offsets_ = aec.offsets().value();
-    }
+    template <typename SimpleValueType>
+    Values decode_all_(const typename AecDecompressor<SimpleValueType>::CompressedData& in_buf, double bscale, double dscale);
 
-    Values values(simple_values.size());
-    std::transform(simple_values.begin(), simple_values.end(), values.begin(), [&](const auto& simple_value) {
-        return (simple_value * bscale + reference_value_) * dscale;
-    });
-    return values;
-  }
-
+private: // members
+    size_t n_elems_;
 };
 } // namespace mc
 
