4efc80cc5b6dba0cdd5f8118e799c881c2e0e786
Chris Bradley <chris.bradley@ecmwf.int>
2025-04-01 16:01:52 +0100

    Remove old extract functions

--- src/gribjump/LocalGribJump.h
+++ src/gribjump/LocalGribJump.h
@@ -36,12 +36,6 @@ public:
 
     std::vector<std::unique_ptr<ExtractionResult>> extract(ExtractionRequests& requests) override;
 
-    // old API
-    std::vector<std::unique_ptr<ExtractionItem>> extract_old(const eckit::PathName& path,
-                                                             const std::vector<eckit::Offset>& offsets,
-                                                             const std::vector<std::vector<Range>>& ranges) override;
-
-
     std::vector<std::unique_ptr<ExtractionResult>> extract(const eckit::PathName& path,
                                                            const std::vector<eckit::Offset>& offsets,
                                                            const std::vector<std::vector<Range>>& ranges) override;
