47492ce38b8a60c3c6285d5e420894a9182deb7e
Chris Bradley <chris.bradley@ecmwf.int>
2025-04-09 11:55:36 +0100

    Update version 0.10.0

--- pygribjump/src/pygribjump/_version.py
+++ pygribjump/src/pygribjump/_version.py
@@ -4,5 +4,4 @@ import importlib.metadata
 
 __version__ = importlib.metadata.version("pygribjump")
 
-# @todo: Update this to 0.10.0
-__min_lib_version__ = "0.9.3" # Latest breaking C-API change
+__min_lib_version__ = "0.10.0" # Latest breaking C-API change
