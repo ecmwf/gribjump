8120be60bba758657a74eaf7756e577bc503a95c
Caragh Bradley <chris.bradley@ecmwf.int>
2026-06-29 16:08:12 +0200

    Bump name

--- src/gribjump/jumper/SimpleJumper.cc
+++ src/gribjump/jumper/SimpleJumper.cc
@@ -8,7 +8,7 @@
  * does it submit to any jurisdiction.
  */
 
-/// @author Christopher Bradley
+/// @author Caragh Bradley
 
 #include "gribjump/jumper/SimpleJumper.h"
 #include "gribjump/GribJumpDataAccessor.h"
--- src/gribjump/remote/WorkQueue.cc
+++ src/gribjump/remote/WorkQueue.cc
@@ -8,7 +8,7 @@
  * does it submit to any jurisdiction.
  */
 
-/// @author Christopher Bradley
+/// @author Caragh Bradley
 
 #include "gribjump/remote/WorkQueue.h"
 
